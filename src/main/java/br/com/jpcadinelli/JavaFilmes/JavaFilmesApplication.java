1da54c5733a60faeeebd7ed237b6f921365b563f
jpcadinelli <jpcadinelli@gmail.com>
2025-10-02 13:07:13 -0300

    first commit

---
+++ src/main/java/br/com/jpcadinelli/JavaFilmes/JavaFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.jpcadinelli.JavaFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jpcadinelli/JavaFilmes/JavaFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpcadinelli.JavaFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
